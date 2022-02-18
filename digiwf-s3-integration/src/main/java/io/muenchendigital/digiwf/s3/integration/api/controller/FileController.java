69d847ccae4788ab1726554fb1ee6a6a5538d5d8
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-02-18 11:28:28 +0100

    add missing validated

--- digiwf-s3-integration/src/main/java/io/muenchendigital/digiwf/s3/integration/api/controller/FileController.java
+++ digiwf-s3-integration/src/main/java/io/muenchendigital/digiwf/s3/integration/api/controller/FileController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Slf4j
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/file")
