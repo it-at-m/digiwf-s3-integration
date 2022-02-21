6b06e04a7736d15a59ca8f254e799b78377a52f0
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-02-21 10:16:52 +0100

    adapt properties

--- digiwf-s3-integration-starter/src/main/java/io/muenchendigital/digiwf/s3/integration/configuration/CronJobConfiguration.java
+++ digiwf-s3-integration-starter/src/main/java/io/muenchendigital/digiwf/s3/integration/configuration/CronJobConfiguration.java
@@ -3,10 +3,12 @@ package io.muenchendigital.digiwf.s3.integration.configuration;
 import io.muenchendigital.digiwf.s3.integration.domain.service.cronjob.CleanUpDatabaseFolderWithoutCorrespondingS3Folder;
 import io.muenchendigital.digiwf.s3.integration.domain.service.cronjob.CleanUpExpiredFolders;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @ConditionalOnProperty(
@@ -22,12 +24,12 @@ public class CronJobConfiguration {
 
     private final CleanUpDatabaseFolderWithoutCorrespondingS3Folder cleanUpDatabaseFolderWithoutCorrespondingS3Folder;
 
-    @Scheduled(cron = "${de.muenchen.documentstorage.cronjob.cleanup.expired-folders}")
+    @Scheduled(cron = "${io.muenchendigital.digiwf.s3.cronjob.cleanup.expired-folders}")
     public void cronJobDefinitionCleanUpExpiredFolders() {
         this.cleanUpExpiredFolders.cleanUp();
     }
 
-    @Scheduled(cron = "${de.muenchen.documentstorage.cronjob.cleanup.database-folder-without-corresponding-s3-folder}")
+    @Scheduled(cron = "${io.muenchendigital.digiwf.s3.cronjob.cleanup.database-folder-without-corresponding-s3-folder}")
     public void cronJobDefinitionCleanUpDatabaseFolderWithoutCorrespondingS3Folder() {
         this.cleanUpDatabaseFolderWithoutCorrespondingS3Folder.cleanUp();
     }
