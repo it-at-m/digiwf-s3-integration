0473af0b59a42f579b7b0724c8b19f4176547fcf
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-02-25 15:07:41 +0100

    add logic and endpoint to update end of life for existing folder

--- digiwf-s3-integration/src/main/java/io/muenchendigital/digiwf/s3/integration/api/controller/FolderController.java
+++ digiwf-s3-integration/src/main/java/io/muenchendigital/digiwf/s3/integration/api/controller/FolderController.java
@@ -1,5 +1,6 @@
 package io.muenchendigital.digiwf.s3.integration.api.controller;
 
+import io.muenchendigital.digiwf.s3.integration.domain.exception.FolderExistanceException;
 import io.muenchendigital.digiwf.s3.integration.domain.service.FolderHandlingService;
 import io.muenchendigital.digiwf.s3.integration.infrastructure.repository.FolderRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,12 +11,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Slf4j
 @RestController
@@ -35,6 +39,21 @@ public class FolderController {
             return ResponseEntity.noContent().build();
         } catch (final Exception exception) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
+        }
+    }
+
+    @PutMapping(value = "/{refId}")
+    @Operation(description = "Updates the end of life attribute in the corresponding database entry for the folder specified in the parameter")
+    public ResponseEntity<Void> updateEndOfLife(@PathVariable @NotEmpty @Size(max = FolderRepository.LENGTH_REF_ID) final String refId,
+                                                @RequestParam final LocalDate endOfLife) {
+        try {
+            log.info("Received a request for updating the end of life of a certain folder.");
+            this.folderHandlingService.updateEndOfLife(refId, endOfLife);
+            return ResponseEntity.ok().build();
+        } catch (final FolderExistanceException exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
+        } catch (final Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
