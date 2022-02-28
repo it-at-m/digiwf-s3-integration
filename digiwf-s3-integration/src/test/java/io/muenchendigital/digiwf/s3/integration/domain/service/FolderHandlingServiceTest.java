c7dd628b08183bf431ad40d6dbedc70c34a87371
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-02-28 09:30:49 +0100

    add unit tests

--- digiwf-s3-integration/src/test/java/io/muenchendigital/digiwf/s3/integration/domain/service/FolderHandlingServiceTest.java
+++ digiwf-s3-integration/src/test/java/io/muenchendigital/digiwf/s3/integration/domain/service/FolderHandlingServiceTest.java
@@ -1,5 +1,6 @@
 package io.muenchendigital.digiwf.s3.integration.domain.service;
 
+import io.muenchendigital.digiwf.s3.integration.domain.exception.FolderExistanceException;
 import io.muenchendigital.digiwf.s3.integration.infrastructure.entity.Folder;
 import io.muenchendigital.digiwf.s3.integration.infrastructure.exception.S3AccessException;
 import io.muenchendigital.digiwf.s3.integration.infrastructure.exception.S3AndDatabaseAsyncException;
@@ -15,10 +16,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -60,6 +65,28 @@ class FolderHandlingServiceTest {
         Mockito.when(this.s3Repository.getFilepathesFromFolder(uuid.toString())).thenReturn(new HashSet<>(List.of(filePath)));
         Mockito.when(this.folderRepository.findByRefId(uuid.toString())).thenReturn(Optional.of(new Folder()));
         Assertions.assertDoesNotThrow(() -> this.folderHandlingService.deleteFolder(uuid.toString()));
+    }
+
+    @Test
+    void updateEndOfLifeException() {
+        final String refId = UUID.randomUUID().toString();
+        Mockito.when(this.folderRepository.findByRefId(refId)).thenReturn(Optional.empty());
+        Assertions.assertThrows(FolderExistanceException.class, () -> this.folderHandlingService.updateEndOfLife(refId, LocalDate.now()));
+
+        Mockito.when(this.folderRepository.findByRefId(refId)).thenReturn(Optional.of(new Folder()));
+        Assertions.assertDoesNotThrow(() -> this.folderHandlingService.updateEndOfLife(refId, LocalDate.now()));
+    }
+
+    @Test
+    void updateEndOfLife() throws FolderExistanceException {
+        final String refId = UUID.randomUUID().toString();
+        final LocalDate newEndOfLife = LocalDate.now().plusYears(1);
+        final Folder folder = new Folder();
+        Mockito.when(this.folderRepository.findByRefId(refId)).thenReturn(Optional.of(folder));
+        this.folderHandlingService.updateEndOfLife(refId, newEndOfLife);
+
+        assertThat(newEndOfLife, is(folder.getEndOfLife()));
+        Mockito.verify(this.folderRepository, Mockito.times(1)).save(folder);
     }
 
 }
