19136b4321536895c15f9a4ae481163dc5287b2f
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-03-04 09:54:10 +0100

    fix bug in example

--- example-s3-integration-client/src/main/java/io/muenchendigital/digiwf/s3/integration/client/S3IntegrationClientApplication.java
+++ example-s3-integration-client/src/main/java/io/muenchendigital/digiwf/s3/integration/client/S3IntegrationClientApplication.java
@@ -2,8 +2,9 @@ package io.muenchendigital.digiwf.s3.integration.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class S3IntegrationClientApplication {
 
     public static void main(final String[] args) {
