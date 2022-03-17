2eea6e098e144c252ee604edb555500e0bb19d6f
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2022-03-17 12:10:14 +0100

    add unit tests

--- digiwf-s3-integration/src/test/java/io/muenchendigital/digiwf/s3/integration/domain/service/FolderHandlingServiceTest.java
+++ digiwf-s3-integration/src/test/java/io/muenchendigital/digiwf/s3/integration/domain/service/FolderHandlingServiceTest.java
@@ -1,63 +1,78 @@
 package io.muenchendigital.digiwf.s3.integration.domain.service;
 
+import io.muenchendigital.digiwf.s3.integration.infrastructure.entity.File;
+import io.muenchendigital.digiwf.s3.integration.infrastructure.exception.S3AccessException;
+import io.muenchendigital.digiwf.s3.integration.infrastructure.exception.S3AndDatabaseAsyncException;
+import io.muenchendigital.digiwf.s3.integration.infrastructure.repository.FileRepository;
+import io.muenchendigital.digiwf.s3.integration.infrastructure.repository.S3Repository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Stream;
+
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 class FolderHandlingServiceTest {
-/*
+
     @Mock
     private S3Repository s3Repository;
 
     @Mock
     private FileRepository fileRepository;
 
+    @Mock
+    private FileHandlingService fileHandlingService;
+
     private FolderHandlingService folderHandlingService;
 
     @BeforeEach
     public void beforeEach() {
-        this.folderHandlingService = new FolderHandlingService(this.s3Repository, this.fileRepository);
+        this.folderHandlingService = new FolderHandlingService(this.s3Repository, this.fileRepository, this.fileHandlingService);
+        Mockito.reset(this.s3Repository);
+        Mockito.reset(this.fileRepository);
+        Mockito.reset(this.fileHandlingService);
     }
 
     @Test
     void deleteFolderException() throws S3AccessException {
-        final UUID uuid = UUID.randomUUID();
-        Mockito.when(this.s3Repository.getFilepathesFromFolder(uuid.toString())).thenReturn(new HashSet<>());
-        Mockito.when(this.fileRepository.findByPathToFile(uuid.toString())).thenReturn(Optional.of(new File()));
-        Assertions.assertThrows(S3AndDatabaseAsyncException.class, () -> this.folderHandlingService.deleteFolder(uuid.toString()));
+        final String pathToFile = "folder/file.txt";
+        final String pathToFolder = "folder";
+        final File file = new File();
+        file.setPathToFile(pathToFile);
 
-        Mockito.when(this.s3Repository.getFilepathesFromFolder(uuid.toString())).thenReturn(new HashSet<>(List.of("test.txt")));
-        Mockito.when(this.fileRepository.findByPathToFile(uuid.toString())).thenReturn(Optional.empty());
-        Assertions.assertThrows(S3AndDatabaseAsyncException.class, () -> this.folderHandlingService.deleteFolder(uuid.toString()));
+        Mockito.when(this.s3Repository.getFilepathesFromFolder(pathToFolder)).thenReturn(new HashSet<>());
+        Mockito.when(this.fileRepository.findByPathToFileStartingWith(pathToFolder)).thenReturn(Stream.of(file));
+        Assertions.assertThrows(S3AndDatabaseAsyncException.class, () -> this.folderHandlingService.deleteFolder(pathToFolder));
 
-        Mockito.when(this.s3Repository.getFilepathesFromFolder(uuid.toString())).thenReturn(new HashSet<>());
-        Mockito.when(this.fileRepository.findByPathToFile(uuid.toString())).thenReturn(Optional.empty());
-        Assertions.assertDoesNotThrow(() -> this.folderHandlingService.deleteFolder(uuid.toString()));
+        Mockito.when(this.s3Repository.getFilepathesFromFolder(pathToFolder)).thenReturn(new HashSet<>(List.of(pathToFile)));
+        Mockito.when(this.fileRepository.findByPathToFileStartingWith(pathToFolder)).thenReturn(Stream.empty());
+        Assertions.assertThrows(S3AndDatabaseAsyncException.class, () -> this.folderHandlingService.deleteFolder(pathToFolder));
+
+        Mockito.when(this.s3Repository.getFilepathesFromFolder(pathToFolder)).thenReturn(new HashSet<>());
+        Mockito.when(this.fileRepository.findByPathToFileStartingWith(pathToFolder)).thenReturn(Stream.empty());
+        Assertions.assertDoesNotThrow(() -> this.folderHandlingService.deleteFolder(pathToFolder));
     }
 
     @Test
     void deleteFolder() throws S3AccessException {
-        final UUID uuid = UUID.randomUUID();
-        final String filePath = uuid.toString().concat("/test.txt");
-        Mockito.when(this.s3Repository.getFilepathesFromFolder(uuid.toString())).thenReturn(new HashSet<>(List.of(filePath)));
-        Mockito.when(this.fileRepository.findByPathToFile(uuid.toString())).thenReturn(Optional.of(new File()));
-        Assertions.assertDoesNotThrow(() -> this.folderHandlingService.deleteFolder(uuid.toString()));
+        final String pathToFile = "folder/file.txt";
+        final String pathToFolder = "folder";
+        final File file = new File();
+        file.setPathToFile(pathToFile);
+
+        Mockito.when(this.s3Repository.getFilepathesFromFolder(pathToFolder)).thenReturn(new HashSet<>(List.of(pathToFile)));
+        Mockito.when(this.fileRepository.findByPathToFileStartingWith(pathToFolder)).thenReturn(Stream.of(file));
+        Assertions.assertDoesNotThrow(() -> this.folderHandlingService.deleteFolder(pathToFolder));
+        Mockito.verify(this.fileHandlingService, Mockito.times(1)).deleteFile(pathToFile);
     }
 
-
-    @Test
-    void updateEndOfLife() throws FolderExistanceException {
-        final String refId = UUID.randomUUID().toString();
-        final LocalDate newEndOfLife = LocalDate.now().plusYears(1);
-        final File folder = new File();
-        Mockito.when(this.fileRepository.findByPathToFile(refId)).thenReturn(Optional.of(folder));
-        this.folderHandlingService.updateEndOfLife(refId, newEndOfLife);
-
-        assertThat(newEndOfLife, is(folder.getEndOfLife()));
-        Mockito.verify(this.fileRepository, Mockito.times(1)).save(folder);
-    }
-*/
 }
